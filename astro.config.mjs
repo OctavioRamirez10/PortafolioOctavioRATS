9eff343e001a1d2ba1c474fc949d745f3bf593c3
Octavio Ramirez <octaaviorami@gmail.com>
2026-05-31 19:10:10 -0300

    Configurar GitHub Pages con Actions y corregir dependencias npm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://OctavioRamirez10.github.io',
+  base: '/PortafolioOctavioRATS',
+});
