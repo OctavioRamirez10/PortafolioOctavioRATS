886fd8592523d0a78a592ac1458897ec2ea8ca2e
Octavio Ramirez <octaaviorami@gmail.com>
2026-06-16 20:47:07 -0300

    Updates

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://OctavioRamirez10.github.io',
-  base: '/PortafolioOctavioRATS/',
-});
